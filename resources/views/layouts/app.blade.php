98ce26191a7d09a6f54bcb1614ec0d8c38db4c15
simonfilth <simonfilth86@hotmail.com>
2017-06-19 19:00:52 -0400

    background sorteo en vivo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,9 @@
 @include('layouts.partials.htmlheader')
+@if(Request::is('sorteo-en-vivo'))
+<body style="background-color: #302f2c;">
+@else
 <body>
+@endif
     <div id="app">
         @include('layouts.partials.nav')
     @if(Auth::check() and Auth::user()->tipo_usuario=='Administrador' and (!Request::is('/') and !Request::is('home')))
--- resources/views/sorteos/sorteo-en-vivo.blade.php
+++ resources/views/sorteos/sorteo-en-vivo.blade.php
@@ -4,8 +4,8 @@
 <div class="col-sm-8">
 	<div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">
-                {{trans('mensajes.sorteo-en-vivo')}}
+            <h1 class="page-header color-blanco">
+                {{trans('mensajes.sorteo-en-vivo')}} <i class="fa fa-youtube"></i>
                 
             </h1>
             <div class="text-success text-center">
@@ -17,17 +17,17 @@
     </div>
     <div class="row">
         <div class="col-sm-12">
-            <div class="panel">
-            	<div class="panel-heading">
-            		<!-- <h2 class="panel-title">
+            <div class="panel" style="border: 1px solid #302f2c; border-radius: 0;">
+            	<!--<div class="panel-heading">
+            		 <h2 class="panel-title">
             			{{trans('mensajes.sorteo-en-vivo')}}
-            		</h2> -->
-            	</div>
+            		</h2> 
+            	</div>-->
             	
-            	<div class="panel-body text-center">
+            	<div class="panel-body text-center" style="background-color: #302f2c;">
                  <iframe width="560" height="315" src="https://gaming.youtube.com/embed/MvDCZAeRnbk" frameborder="0" allowfullscreen></iframe>  
                  <br>
-                 <iframe width="480" height="270" src="https://gaming.youtube.com/live_chat?v=MvDCZAeRnbk&embed_domain=http://rifa-bitcoins.dev/sorteo-en-vivo" frameborder="0" allowfullscreen=""></iframe>   
+                 <!-- <iframe width="480" height="270" src="https://gaming.youtube.com/live_chat?v=MvDCZAeRnbk&embed_domain=http://rifa-bitcoins.dev/sorteo-en-vivo" frameborder="0" allowfullscreen=""></iframe>    -->
 
                 </div>
             </div>
